8b618c8eb25a1c61778493a22c4c6a9502c17479
Tomvis <stereotom@gmail.com>
2020-03-10 11:53:14 +0200

    adding my keymap

---
+++ keyboards/ergodox_ez/keymaps/Tomvis/config.h
@@ -0,0 +1,11 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 6
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 70
+
